4d085d41cd5fea652f18ffc3906ad3b45e86e72a
Abdurrahman Abdul Hamid <74585156+Artcods@users.noreply.github.com>
2024-10-13 13:20:13 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,54 +4,5 @@ using namespace std;
 
 int main()
 {
-    List L;
-    cout << "first(L) sebelum createList : " << first(L) << endl;
-
-    Create_List(L);
-    cout << "first(L) setelah createList : " << first(L) << endl;
-    cout << endl;
-
-    // Memanggil fungsi new element
-    adr P;
-    P = newElement(10);
-    cout <<"info(P) : "<< info(P) << endl;
-    cout <<"next(P) : "<< next(P) << endl;
-
-    // insert first
-    cout << "first(L) sebelum insertFirst : " << first(L) << endl;
-    Insert_first(L, P);
-    cout << "first(L) setelah insertFirst : " << first(L) << endl;
-    cout << "info First (L) : " << info(first(L)) << endl;
-
-    P = newElement(100);
-    Insert_first(L, P);
-    cout << "info First(L) : " << info(first(L)) << endl;
-    cout << endl;
-
-    // show
-    Show(L);
-
-    P = newElement(230);
-    Insert_first(L, P);
-    cout << endl;
-
-    Show(L);
-    cout << endl;
-
-    // Delete Last
-    P = Delete_Last(L);
-    Show(L);
-    cout << info(P) << endl;
-    cout << endl;
-
-    Delete_Last(L);
-    Show(L);
-    cout << info(P) << endl;
-    cout << endl;
-
-    Delete_Last(L);
-    Show(L);
-    cout << info(P) << endl;
-
     return 0;
 }
